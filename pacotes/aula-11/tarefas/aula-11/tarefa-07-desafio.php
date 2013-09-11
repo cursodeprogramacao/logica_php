21446516882095bf7b1698e972d5685a8469190e
Harlley Oliveira <harlley@gmail.com>
2013-09-11 20:48:25 -0300

    aula 11

---
+++ pacotes/aula-11/tarefas/aula-11/tarefa-07-desafio.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+/********************************************************************** 
+	Tarefa 7 - Desafio: (USE FOR)
+		Refaça o código da tarefa 3 usando o FOR ao invés de WHILE
+***********************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
